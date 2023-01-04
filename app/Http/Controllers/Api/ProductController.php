cb1a645dacc3040d854eb740512d85beb54cc0c9
tsakib360 <tsakib360@gmail.com>
2023-01-04 18:17:01 +0600

    instock image add in product collection endpoint.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -48,12 +48,22 @@ class ProductController extends Controller
         if(!is_null($request->get('limit'))) {
             $products= tap(Product::latest()->with('shop', 'size', 'instock')->paginate($request->limit)->appends('limit', $request->limit))->transform(function($product){
                 $product['size']= $product->size;
+                if(!is_null($product->instock)) {
+                    $images=collect();
+                    foreach ($product->instock->getMedia('product') as $img){
+                        $images->push($img->getFullUrl());
+                    }
+                    $product['instock']['images']= $images;
+                    unset($product['instock']['media']);
+                }
+
                 unset($product['size_id']);
                 return $product;
             });
         }else{
             $products= Product::latest()->with('shop', 'size', 'instock')->get()->map(function($product){
                 $product['size']= $product->size;
+                $product['image']= $product->instock;
                 unset($product['size_id']);
                 return $product;
             });
